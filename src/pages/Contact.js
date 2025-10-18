c2a73099f4bb20499cdd2abff31f209d9b0754dd
SNIPL-1 <kjindal123@gmail.com>
2025-10-18 13:18:40 +0530

    Add Contact component with contact details

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <h2>Contact Us</h2>
+      <p>SCO 15, Sia Homes, Lalton Chowk, Pakhowal Road, Ludhiana - 142022</p>
+      <p>Phone: +91-7986297302</p>
+      <p>Email: export@srineelkanth.com</p>
+      <p>
+        <a href="https://www.instagram.com/grotech_tools/">Instagram</a> | 
+        <a href="https://www.facebook.com/profile.php?id=61571301311078"> Facebook</a>
+      </p>
+    </div>
+  );
+}
