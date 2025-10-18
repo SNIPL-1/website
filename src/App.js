918a9ea09284f77ee63b9c1fe5621fc99d14fcec
SNIPL-1 <kjindal123@gmail.com>
2025-10-18 13:11:56 +0530

    Initialize App component with routing setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Enquiry from './pages/Enquiry';
+import Contact from './pages/Contact';
+import Cart from './components/Cart';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/enquiry" element={<Enquiry />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
